74669b3e360ba7193111bff329d8978ae6027fcd
Andre Telfer <andre.telfer@sickkids.ca>
2025-04-14 10:11:38 -0400

    Remove build dir

--- services/backend/build/lib/api/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class ButtonPress(models.Model):
-    id = models.AutoField(primary_key=True)
-    count = models.IntegerField(default=0)
-    last_pressed = models.DateTimeField(auto_now=True, db_index=True)
--- services/backend/build/lib/api/tasks.py
+++
@@ -1,9 +0,0 @@
-import logging 
-
-from celery import shared_task
-
-logger = logging.getLogger('django')
-
-@shared_task
-def log_hello(key):
-    logger.info("Hello, world!")
